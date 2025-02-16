0bfe021ef6c22000638d42cdb54cd89aeac7b43b
EJJBR <edgardobarboza94@gmail.com>
2025-02-15 21:48:21 -0500

    Agregando las dependencias para las conexiones remotas y fijando la app en modo vertical

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.cardview)
+    implementation("com.android.volley:volley:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
