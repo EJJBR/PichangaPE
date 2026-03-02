8a993b2b497f364ede7b930a6db3994d62fbfd06
EJJBR <edgardobarboza94@gmail.com>
2026-03-02 00:03:41 -0500

    Subiendo configuracion para subida de imagenes a imgBB

--- app/src/main/java/com/example/pichangape/ApiConfig.java
+++ app/src/main/java/com/example/pichangape/ApiConfig.java
@@ -1,6 +1,9 @@
 package com.example.pichangape;
 
 public class ApiConfig {
-    // Si cambias de servidor (a localhost o a otro Replit), solo tendrás que cambiar esta línea
+    // URL base de tu API en Replit
     public static final String BASE_URL = "https://1fe8107b-4bc6-4865-9bbd-dbd93570a5ba-00-z75lvfccgfim.worf.replit.dev/";
+
+    // Clave de API de ImgBB para la subida de vouchers (Rol Cliente)
+    public static final String IMGBB_API_KEY = "7a6806686ae5fc9ccc35163b58cc17aa";
 }
